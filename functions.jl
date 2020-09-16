8266062cdc3203eb949c8a69824fe68100e0b063
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-09-16 12:53:30 -0400

    updates

--- functions.jl
+++ functions.jl
@@ -1,10 +1,37 @@
-# Creating a function to process the imputation
-function imputation(data, rank, rows, cols)
-    data[rows, cols] = 0.1
-    for iterations in 1:1
-        SVD = svd(data)
-        SVD.S[rank:end] .= 0.0
-        data = SVD.U*Diagonal(SVD.S)*transpose(SVD.V)
+"""
+    lowrank(N, r)
+
+Returns the low rank appproximation of the matrix N in rank r, after a SVD.
+"""
+function lowrank(N, r)
+    @assert r ≤ rank(N)
+    F = svd(N)
+    F.S[(r + 1):end] .= zero(eltype(F.S))
+    # Note that the SVD method returns Vt, which is the transposed right singular matrix
+    return F.U * Diagonal(F.S) * F.Vt
+end
+
+"""
+    imputation(N, position, i0; rank=3, tol=1e-2, maxiter=50)
+
+This function returns the imputed value at a given position (expressed in
+CartesianCoordinates) in the matrix N, seeded from a value i0. The imputation
+is done by iterating an SVD at a given rank, and stops when the iteration
+difference is smaller than the absolute tolerance, of after maxiter steps
+have been done.
+"""
+function imputation(N, position, i0; rank = 3, tol = 1e-2, maxiter = 50)
+    N[position] = i0
+    Δ = 1.0
+    iter = 1
+    while Δ > tol
+        iter += 1
+        Z = lowrank(N, rank)
+        # The change in value is the absolute difference between the next and current iteration
+        Δ = abs(Z[position] - N[position])
+        N[position] = Z[position]
+        # We stop if there are more than a set number of iterations
+        iter ≥ maxiter && break
     end
-    return data[rows, cols]
+    return N[position]
 end
--- run.jl
+++ run.jl
@@ -2,55 +2,36 @@ using CSV
 using DataFrames
 using Plots
 using LinearAlgebra
+using Statistics
+using ProgressMeter
+
 include("functions.jl")
 
 # Reading the data file
 df = CSV.read("virionette.csv");
-# Creating a Dictionary and Attributing host ID's
-Dictionary_host = Dict(unique(df.host_species)[h] => h for h in 1:length(unique(df.host_species)));
 
-# Creating a Dictionary and Attributing virus ID's
-Dictionary_virus = Dict(unique(df.virus_genus)[v-length(unique(df.host_species))] => v for v in length(unique(df.host_species))+1:length(unique(df.virus_genus))+length(unique(df.host_species)));
-## Creating an interactions matrix
-# Creating a matrix of zeros
-Matrix_interactions = zeros(Float64, length(unique(df.host_species))+1, length(unique(df.virus_genus))+1);
+# Make a sorted list of unique hosts and viruses
+const hosts = sort(unique(df.host_species))
+const viruses = sort(unique(df.virus_genus))
 
-# Assigning IDs for rows
-for r in 2:length(unique(df.host_species))+1
-    Matrix_interactions[r,1] = Dictionary_host[unique(df.host_species)[r-1]]
+# Use this information to fill the matrix
+M = zeros(Float64, (length(viruses), length(hosts)))
+for interaction in eachrow(df)
+    host_idx = findfirst(hosts .== interaction.host_species)
+    virus_idx = findfirst(viruses .== interaction.virus_genus)
+    M[virus_idx, host_idx] = 1.0
 end
-
-# Assigning IDs for cols
-for c in 2:length(unique(df.virus_genus))+1
-    Matrix_interactions[1,c] = Dictionary_virus[unique(df.virus_genus)[c-1]]
-end
-
-Matrix_interactions
-
-# Fill matrix with ones where there are interactions
-for interactions in 1:length(df.host_species)
-    row_ID = Dictionary_host[df.host_species[interactions]]
-    col_ID = Dictionary_virus[df.virus_genus[interactions]]
-    Matrix_interactions[row_ID + 1, col_ID - length(unique(df.host_species)) + 1] = 1.0
-end
-
-# Selecting only the data in the matrix
-const Matrix_interactions_data = Matrix_interactions[2:end, 2:end];
 
 # Visualizing the interactions
-heatmap(Matrix_interactions_data , xlabel = "Virus", ylabel="Host")
+heatmap(M, xlabel = "Hosts", ylabel = "Viruses", c = :Greys, leg = false, frame = :box)
 
-# Creating a copy of the matrix data
-new_Matrix = copy(Matrix_interactions_data);
+# Do the imputation for every zero value
+positions_to_impute = findall(M .== 0.0)
+K = copy(M)
 
-# Iterating through each missing interactions for the imputation
-for rows in 1:length(unique(df.host_species))
-    for cols in 1:length(unique(df.virus_genus))
-        if Matrix_interactions_data[rows, cols] == 0.0
-            new_Matrix[rows, cols] = imputation(Matrix_interactions_data, 5, rows, cols)
-        end
-    end
+@showprogress for p in positions_to_impute
+    K[p] = imputation(M, p, mean(M))
 end
 
-# Visualizing the new interactions
-heatmap(new_Matrix, xlabel = "Virus", ylabel="Host")
+# Visualizing the interactions
+heatmap(K, xlabel = "Hosts", ylabel = "Viruses", c = :Greys, leg = false, frame = :box)
