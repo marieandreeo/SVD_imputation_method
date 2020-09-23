1cd8f364a664cd7c95f0eb9fef8e2ca3e572f216
Marie-Andrée Ouellet <ouellet.marieandree@gmail.com>
2020-09-22 21:43:17 -0400

    Added variation calculation

--- src/functions.jl
+++ src/functions.jl
@@ -3,12 +3,11 @@
 
 Reads the CSV data file and return the dataframe, hosts and viruses.
 """
-
 function getData()
 
     # Reading the data file
-    #df = CSV.read("./data/virionette.csv");
     df = CSV.read("./data/virionette.csv");
+    #df = CSV.read("./data/TestData.csv");
 
     # Make a sorted list of unique hosts and viruses
     hosts = sort(unique(df.host_species))
@@ -16,7 +15,6 @@ function getData()
 
     return df, hosts, viruses
 end
-
 
 """
     buildInteractionMatrix()
@@ -35,13 +33,8 @@ function buildInteractionMatrix()
         interaction_matrix[virus_idx, host_idx] = 1.0
     end
 
-    # Visualizing the interactions
-    heatmap(interaction_matrix, xlabel = "Hosts", ylabel = "Viruses",
-        c = :Greys, leg = false, frame = :box, title = "Initial matrix")
-
     return interaction_matrix
 end
-
 
 """
     crossValidation(interaction_matrix, targetedValue, initialValue, rank)
@@ -50,6 +43,9 @@ Computes the imputation for a given matrix if the targeted value is 0
 Computes the leave one out validation for a given matrix if the targeted value is 1
 """
 function crossValidation(targetedValue, initialValue, rank)
+    println("Targeted Value: $(targetedValue) ",
+        "Initial Value: $(initialValue) ", "Rank: $(rank)")
+
     interaction_matrix = buildInteractionMatrix()
     # Do the imputation for every targeted value
     positions_to_impute = findall(interaction_matrix .== targetedValue)
@@ -59,31 +55,12 @@ function crossValidation(targetedValue, initialValue, rank)
         output_matrix[position] = imputation(interaction_matrix,
             position, initialValue, rank)
     end
-
     # Visualizing the interactions
-    heatmap(output_matrix, xlabel = "Hosts", ylabel = "Viruses", c = :Greys, leg = false, frame = :box, title = "Output matrix")
+    display(plot(generateHeatmap("Initial Matrix", interaction_matrix),
+        generateHeatmap("Output matrix", output_matrix,)))
 
-    println("Targeted Value:$(targetedValue) ", "Initial Value:$(initialValue) ", "Rank:$(rank)")
-    #println("$((1.0-(sum(abs.(interaction_matrix .- output_matrix))/length(interaction_matrix))))%")
-
-    return(output_matrix)
+    println("Variation: $(calculateVariation(interaction_matrix, output_matrix))%")
 end
-
-
-"""
-    lowrank(matrix, svd_rank)
-
-Returns the low rank appproximation of the matrix in rank, after a SVD.
-"""
-function lowrank(matrix, svd_rank)
-    @assert svd_rank ≤ rank(matrix)
-    factorization = svd(matrix)
-    factorization.S[(svd_rank + 1):end] .= zero(eltype(factorization.S))
-    # Note that the SVD method returns Vt, which is the transposed
-    # right singular matrix
-    return factorization.U * Diagonal(factorization.S) * factorization.Vt
-end
-
 
 """
     imputation(matrix, position, initialValue, rank; tolerance=1e-2, maxiter=50)
@@ -95,18 +72,55 @@ difference is smaller than the absolute tolerance, or after maxiter steps
 have been done.
 """
 function imputation(matrix, position, initialValue, rank; tolerance = 1e-2, maxiter = 50)
-    matrix[position] = initialValue
+    tempMatrix = copy(matrix)
+    tempMatrix[position] = initialValue
     Δ = 1.0
     iter = 1
     while Δ > tolerance
         iter += 1
-        approx_matrix = lowrank(matrix, rank)
+        approx_matrix = lowrank(tempMatrix, rank)
         # The change in value is the absolute difference between the
         # next and current iteration
-        Δ = abs(approx_matrix[position] - matrix[position])
-        matrix[position] = approx_matrix[position]
+        Δ = abs(approx_matrix[position] - tempMatrix[position])
+        tempMatrix[position] = approx_matrix[position]
         # We stop if there are more than a set number of iterations
         iter ≥ maxiter && break
     end
-    return matrix[position]
+    return tempMatrix[position]
+end
+
+"""
+    lowrank(matrix, svd_rank)
+
+Returns the low rank appproximation of the matrix, after a SVD.
+"""
+function lowrank(matrix, svd_rank)
+    @assert svd_rank ≤ rank(matrix)
+    factorization = svd(matrix)
+    factorization.S[(svd_rank + 1):end] .= zero(eltype(factorization.S))
+    # Note that the SVD method returns Vt, which is the transposed
+    # right singular matrix
+    return factorization.U * Diagonal(factorization.S) * factorization.Vt
+end
+
+"""
+    generateHeatMap(title, matrix)
+
+Returns a heatmap based on the given matrix.
+"""
+function generateHeatmap(title, matrix)
+
+    return heatmap(matrix, xlabel = "Hosts", ylabel = "Viruses", c = :Greys,
+        leg = true, frame = :box, title = title)
+
+end
+
+"""
+    calculateVariation(initial_matrix, output_matrix)
+
+Calculates the variation between two matrices.
+"""
+function calculateVariation(initial_matrix, output_matrix)
+    delta = sum(abs.(initial_matrix .- output_matrix))
+    return round((delta/length(initial_matrix)) * 100, digits = 2);
 end
